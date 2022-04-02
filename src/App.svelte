157b4bf33e337218120b7b4dcf98174fff15c564
Borys35 <boryskac10@gmail.com>
2022-04-02 23:52:54 +0200

    Initial commit with svelte and vite

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import Calculator from "./lib/calculator/Calculator.svelte";
+</script>
+
+<main>
+  <Calculator />
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+
+  :global(body) {
+    margin: 0;
+    padding: 0;
+  }
+
+  main {
+    padding: 2rem;
+  }
+
+  @media (min-width: 768px) {
+    main {
+      padding: 3rem 6rem;
+    }
+  }
+</style>
---
+++ src/lib/calculator/Calculator.svelte
@@ -0,0 +1,15 @@
+<script>
+  import CalculatorKeyboard from "./CalculatorKeyboard.svelte";
+  import CalculatorOutput from "./CalculatorOutput.svelte";
+</script>
+
+<div class="grid">
+  <CalculatorOutput />
+  <CalculatorKeyboard />
+</div>
+
+<style>
+  .grid {
+    display: grid;
+  }
+</style>
